4d36088473b23b9cc18883d1a64a6f7972fadb74
solaleh81 <solalehalizadeh81.com>
2023-10-29 20:25:34 +0330

    adding listing_list view.

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Listing
 
-# Create your views here.
+def listing_list(request):
+    listings = Listing.objects.all()
+    return render(request, 'listings.html', context={'listings': listings})
+
+
