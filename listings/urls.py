681925477c566029d52c54a7f735129fcadb2b6d
solaleh81 <solalehalizadeh81.com>
2023-10-29 22:46:50 +0330

    adding listing-create view.

--- listings/urls.py
+++ listings/urls.py
@@ -6,5 +6,6 @@ from listings import views
 urlpatterns = [
     path('', views.listing_list, name="list"),
     path('<int:id>/', views.listing_retrieve, name="listing_retrieve"),
+    path('add-listing/', views.listing_create, name="listing_create"),
 
 ]
