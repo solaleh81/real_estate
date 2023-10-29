710c094c8f2b91018324769e4ddedd14b1967f74
solaleh81 <solalehalizadeh81.com>
2023-10-29 22:16:18 +0330

    adding listing_retrieve view.

--- listings/views.py
+++ listings/views.py
@@ -5,4 +5,6 @@ def listing_list(request):
     listings = Listing.objects.all()
     return render(request, 'listings.html', context={'listings': listings})
 
-
+def listing_retrieve(request, id):
+    listing = Listing.objects.get(pk=id)
+    return render(request, 'listing.html', context={'listing': listing})
