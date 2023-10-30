fd5f2fa0ef608524687d855574498a9c9546e192
solaleh81 <solalehalizadeh81.com>
2023-10-30 15:26:02 +0330

    adding listing_update url.

--- listings/urls.py
+++ listings/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.listing_list, name="list"),
     path('<int:id>/', views.listing_retrieve, name="listing_retrieve"),
     path('add-listing/', views.listing_create, name="listing_create"),
+    path('<int:id>/edit/', views.listing_update, name="listing_update"),
 
 ]
